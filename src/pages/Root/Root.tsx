2fc790cdaffeb8a38eb791082c0e430630cd3ea2
Krystian Gryczon <me@example.com>
2021-03-06 20:06:09 +0100

    prettier

--- src/pages/Root/Root.tsx
+++ src/pages/Root/Root.tsx
@@ -1,75 +1,49 @@
-import React from "react";
-import Breadcrumb from "antd/lib/breadcrumb/Breadcrumb";
-import Layout, { Content, Footer, Header } from "antd/lib/layout/layout";
-import Sider from "antd/lib/layout/Sider";
-import Menu from "antd/lib/menu";
-import SubMenu from "antd/lib/menu/SubMenu";
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons'
+import './Root.scss';
+import { useState} from "react";
 
-import App from "../App/App";
+import RoomBar from "../../utils/RoomBar"
+
+import { Checkbox, Input } from 'antd';
 
 function Root() {
-  return (
-      <Layout >
-        <Header className="header">
-          <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
-          </Menu>
-        </Header>
-        <Content style={{ margin: "0 auto", padding: "0 50px", maxWidth: '1600px' }}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <Layout
-            className="site-layout-background"
-            style={{ padding: "24px 0" }}
-          >
-            <Sider className="site-layout-background" width={200}>
-              <Menu
-                mode="inline"
-                defaultSelectedKeys={["1"]}
-                defaultOpenKeys={["sub1"]}
-                style={{ height: "100%" }}
-              >
-                <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-                  <Menu.Item key="1">option1</Menu.Item>
-                  <Menu.Item key="2">option2</Menu.Item>
-                  <Menu.Item key="3">option3</Menu.Item>
-                  <Menu.Item key="4">option4</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-                  <Menu.Item key="5">option5</Menu.Item>
-                  <Menu.Item key="6">option6</Menu.Item>
-                  <Menu.Item key="7">option7</Menu.Item>
-                  <Menu.Item key="8">option8</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                  key="sub3"
-                  icon={<NotificationOutlined />}
-                  title="subnav 3"
-                >
-                  <Menu.Item key="9">option9</Menu.Item>
-                  <Menu.Item key="10">option10</Menu.Item>
-                  <Menu.Item key="11">option11</Menu.Item>
-                  <Menu.Item key="12">option12</Menu.Item>
-                </SubMenu>
-              </Menu>
-            </Sider>
-            <Content style={{ padding: "0 24px", minHeight: 280 }}>
-              Content
-            </Content>
-          </Layout>
-        </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Ant Design ©2018 Created by Ant UED
-        </Footer>
-      </Layout>
-  );
+	let taggesy = ["jebac", "disa"];
+  return(
+    <div className="wrapper">
+      <div className="container">
+        <header>
+          <div className="logo"></div>
+          <div className="headerMenu">
+            <button className="headerMenu__item play">Play</button>
+            <button className="headerMenu__item login">Login</button>
+            <button className="headerMenu__item about">About Us</button>
+          </div>
+        </header>
+
+        <section>
+			<Input placeholder="Room's name" />
+			<div className="genres">
+				<Checkbox>Pop</Checkbox>
+				<Checkbox>Rock</Checkbox>
+				<Checkbox>Country</Checkbox>
+				<Checkbox>HipHop</Checkbox>
+			</div>
+        </section>
+
+		<main>
+			<div className="labelBar">
+				<label>Room ID</label>
+				<label>Players</label>
+				<label>Tags</label>
+			</div>
+			<div className="roomList">
+				<RoomBar name="room1534354131544" players="3/5" tags={taggesy} />
+				<RoomBar name="room1115541315d5" players="5/5" />
+				<RoomBar name="pszemkapokuj_hehe" players="4/5" />
+			</div>
+		</main>
+      </div>
+    </div>
+  )
 }
 
 export default Root;
---
+++ src/utils/RoomBar.tsx
@@ -0,0 +1,20 @@
+import './RoomBar.css';
+interface RoomInfo {
+	name: string;
+	players: string;
+	tags?: string[];
+}
+export default function Bar(props:RoomInfo){
+  return(
+    <div className="room">
+      <div>{props.name}</div>
+      <div>{props.players}</div>
+      <div className="tags">
+		{props?.tags ? <div className="tag">{props.tags}</div> : '' }
+        <div className="tag">Funk</div>
+        <div className="tag">Rock</div>
+      </div>
+    </div>
+  )
+}
+
