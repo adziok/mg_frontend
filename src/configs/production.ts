cf6a0e52512475e62b231838eab28e18bbad7282
Adrian Karwowski <adrian.karwowski@elpassion.pl>
2021-03-23 22:28:33 +0100

    a

--- src/configs/production.ts
+++ src/configs/production.ts
@@ -1,4 +1,4 @@
 export const productionConfig = {
-    baseUrl: 'https://music-gueser.herokuapp.com/api/',
-    baseWs: 'wss://music-gueser.herokuapp.com/api/',
+    baseUrl: 'https://music-guesser.herokuapp.com/api/',
+    baseWs: 'wss://music-guesser.herokuapp.com/api/',
 };
